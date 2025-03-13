2c76a4dde75d23f54fb7d7fac9d3e5f690178ddc
niloy <niloy2931@gmail.com>
2025-03-14 01:14:04 +0600

    little changed in the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import FeaturesSection from "./components/FeatureSection";
-import TestimonialSection from "./components/TestimonialSection";
-
 function App() {
-  return (
-    <>
-      <TestimonialSection />
-      <FeaturesSection />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
