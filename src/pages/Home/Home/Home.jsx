9d155d05d02f8c8c6431d6663f852791a6f5cf33
rejaul <rejaulvoice@gmail.com>
2025-03-14 21:06:45 +0600

    pulled the merged update and changes adjust few things

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../../../components/Hero";
 import FeaturesSection from "../../../components/FeaturesSection";
 import ResumeInsightsSection from "../../../components/ResumeInsightsSection";
 import TestimonialSection from "../../../components/TestimonialSection";
+import Feature2 from "../../../components/Feature2";
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <FeaturesSection />
             <ResumeInsightsSection />
             <TestimonialSection />
+            <Feature2 />
         </div>
     );
   // return (
