71b066f80fa3fbe2435220c5a5c25582c08c04ec
niloy <niloy2931@gmail.com>
2025-04-17 20:35:11 +0600

    fixed conflict

--- src/layout/dashboard/resume/components/forms/Summery.jsx
+++ src/layout/dashboard/resume/components/forms/Summery.jsx
@@ -12,19 +12,11 @@ const prompt =
   "Job Title: {jobTitle} , Depends on job title give me list of  summery for 3 experience level, Mid Level and Freasher level in 3 -4 lines in array format, With summery and experience_level Field in JSON Format";
 
 const Summery = ({ enableNext }) => {
-<<<<<<< HEAD
   const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
   const [summery, setSummery] = useState();
   const [loading, setLoading] = useState(false);
   const params = useParams();
   const [aiGeneratedSummeryList, setAiGeneratedSummeryList] = useState([]);
-=======
-    const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext)
-    const [summery, setSummery] = useState()
-    const [loading, setLoading] = useState(false);
-    const params = useParams();
-    const [aiGeneratedSummeryList, setAiGenerateSummeryList] = useState([]);
->>>>>>> 5acd7e537116f3fb06578c1c21092220322c2622
 
   console.log("AI Summary List:", aiGeneratedSummeryList);
   console.log("Type:", typeof aiGeneratedSummeryList);
@@ -51,7 +43,6 @@ const Summery = ({ enableNext }) => {
     const PROMPT = prompt.replace("{jobTitle}", resumeInfo?.jobTitle);
     console.log(PROMPT);
 
-<<<<<<< HEAD
     const result = await AIChatSession.sendMessage(PROMPT);
     const responseText = await result.response.text(); // properly await text()
 
@@ -87,83 +78,6 @@ const Summery = ({ enableNext }) => {
       (error) => {
         setLoading(false);
       }
-=======
-    const GenerateSummeryFromAI = async () => {
-        setLoading(true)
-        const PROMPT = prompt.replace('{jobTitle}', resumeInfo?.jobTitle);
-        console.log(PROMPT);
-        const result = await AIChatSession.sendMessage(PROMPT);
-        console.log(JSON.parse(result.response.text()))
-
-        // setAiGenerateSummeryList([JSON.parse(result.response.text())])
-        const parsed = JSON.parse(result.response.text());
-        setAiGenerateSummeryList(parsed.summaries);
-
-        setLoading(false);
-    }
-
-    const onSave = (e) => {
-        e.preventDefault();
-
-        setLoading(true)
-        const data = {
-            data: {
-                summery: summery
-            }
-        }
-        GlobalApi.UpdateResumeDetail(params?.resumeId, data).then(resp => {
-            console.log(resp);
-            enableNext(true);
-            setLoading(false);
-            toast("Details updated")
-        }, (error) => {
-            setLoading(false);
-        })
-    }
-    return (
-        <div>
-            <div className='p-5 shadow-lg shadow-gray-400 rounded-lg border-t-gray-400 border-t-4 mt-10'>
-                <h2 className='font-bold text-lg text-white'>Summery</h2>
-                <p className='text-white'>Add Summery for your job title</p>
-
-                <form className='mt-7' onSubmit={onSave}>
-                    <div className='flex justify-between items-end'>
-                        <label className='text-white'>Add Summery</label>
-                        <Button type="button" variant="outline" size="sm"
-                            onClick={() => GenerateSummeryFromAI()}
-                            className="border-primary text-primary gap-2">
-                            <Brain className='h-4 w-4' /> Generate from AI</Button>
-                    </div>
-                    <Textarea className="mt-5 text-white" required
-                        value={summery}
-                        onChange={(e) => setSummery(e.target.value)}
-                    />
-                    <div className='mt-2 flex justify-end'>
-                        <Button type="submit" variant="outline"
-                            disabled={loading}>
-                            {loading ? <LoaderCircle className='animate-spin' /> : 'Save'}
-                        </Button>
-                    </div>
-                </form>
-            </div>
-
-            {Array.isArray(aiGeneratedSummeryList) && aiGeneratedSummeryList.length > 0 && (
-                <div className='my-5'>
-                    <h2 className='font-bold text-lg'>Suggestions</h2>
-                    {aiGeneratedSummeryList.map((item, index) => (
-                        <div key={index}
-                            onClick={() => setSummery(item?.summary)}
-                            className='p-5 shadow-lg my-4 rounded-lg cursor-pointer'>
-                            <h2 className='font-bold my-1 text-primary'>Level: {item?.experience_level}</h2>
-                            <p>{item?.summary}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
-
-
-        </div>
->>>>>>> 5acd7e537116f3fb06578c1c21092220322c2622
     );
   };
   return (
--- src/layout/dashboard/resume/components/preview/ExperiencePreview.jsx
+++ src/layout/dashboard/resume/components/preview/ExperiencePreview.jsx
@@ -11,7 +11,6 @@ const ExperiencePreview = ({ resumeInfo }) => {
       </h2>
       <hr style={{ borderColor: resumeInfo?.themeColor }} />
 
-<<<<<<< HEAD
       {resumeInfo?.experience.map((experience, idx) => (
         <div key={idx} className="my-5">
           <h2
@@ -36,25 +35,6 @@ const ExperiencePreview = ({ resumeInfo }) => {
             className="text-white text-xs"
             dangerouslySetInnerHTML={{ __html: experience?.workSummery }}
           ></div>
-=======
-            {resumeInfo?.experience.map((experience, idx) => (
-                <div key={idx} className='my-5'>
-                    <h2 className='text-white text-sm font-bold'
-                    style={{
-                        color:resumeInfo?.themeColor
-                    }}
-                    >{experience?.title}</h2>
-                    <h2 className='text-white text-xs flex justify-between'>{experience?.companyName}, 
-                        {experience?.city}, {experience?.state}
-                        <span>{experience?.startDate} To {experience?.currentlyWorking? 'Present' : experience?.endDate}</span>
-                        </h2>
-                        {/* <p className='text-white text-xs my-2'>
-                            {experience?.workSummery}
-                        </p> */}
-                        <div className='text-white' dangerouslySetInnerHTML={{__html:experience?.workSummery}} />
-                </div>
-            ))}
->>>>>>> 5acd7e537116f3fb06578c1c21092220322c2622
         </div>
       ))}
     </div>
