6909dd4fe05ea9b59c4abde874426fe883c44bba
ActiveShayun <wevdeveloper0477gmail.com>
2025-04-20 20:36:55 +0600

    some chance

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -11,4 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
