471b8e6de472b523d644220a32d56d983c6a3319
niloy <niloy2931@gmail.com>
2025-04-21 22:08:42 +0600

    merge this

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import Contact from './Contact';
-
+import React from "react";
+import Contact from "./Contact";
+import NavBar from "@/shared/NavBar/NavBar";
 
 const ContactUs = () => {
-    return (
-        <div className='w-[100%] bg-black  min-h-screen'>
-           <Contact></Contact>
-        </div>
-    );
+  return (
+    <div className="w-[100%] bg-black  min-h-screen">
+      <NavBar></NavBar>
+      <Contact></Contact>
+    </div>
+  );
 };
 
 export default ContactUs;
